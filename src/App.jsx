71cd25ef97980a6d0571c5df7bea4b95d2466e42
LuckyPride <IsmaeelnKhan@gmail.com>
2025-03-17 17:19:57 -0700

    add login component with styling and js-cookie integration

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './Login/Login.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-    </>
+    <div>
+      <Login/>
+    </div>
   )
 }
-
 export default App
