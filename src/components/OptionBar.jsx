28faba3fe5c36fcc897ffe7038f55faa4d3a4813
William Scott <wsc0tt@outlook.com>
2025-08-27 19:28:33 -0700

    fix

--- src/components/OptionBar.jsx
+++ src/components/OptionBar.jsx
@@ -1,23 +1,22 @@
 // Need this to manage login and logout states
-import React, { useState } from 'react'; 
+import React, { useState } from 'react'
 // mui containers
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
 // display icons
-import HomeIcon from '@mui/icons-material/Home';
-import WorkIcon from '@mui/icons-material/Work';
-import LoginIcon from '@mui/icons-material/Login';
-import LogoutIcon from '@mui/icons-material/Logout';
+import HomeIcon from '@mui/icons-material/Home'
+import WorkIcon from '@mui/icons-material/Work'
+import LoginIcon from '@mui/icons-material/Login'
+import LogoutIcon from '@mui/icons-material/Logout'
 // mui avatar for login logout
-import Avatar from '@mui/material/Avatar';
+import Avatar from '@mui/material/Avatar'
 // pictures as objects
-import logo from '../assets/maritimelogo5.png';
-import tempAccountPic from '../assets/tom.jpg';
+import logo from '../assets/maritimelogo5.png'
+import tempAccountPic from '../assets/tom.jpg'
 // styles file
-import './OptionBar.css';
+import './OptionBar.css'
 // import link to nav to internal pages
-import { Link } from 'react-router-dom'; 
-
+import { Link } from 'react-router-dom'
 
 // array for center nav options
 const nav_Items = [
@@ -29,10 +28,10 @@ const nav_Items = [
     {
         text: 'Job Board',
         icon: <WorkIcon className='navBarIcon' />,
-        to: '/board', // on click go to job board
+        to: '/fsb', // on click go to job board
     },
     /*add more here*/
-];
+]
 
 /*structure of option bar:
         optionBar
@@ -48,18 +47,20 @@ const nav_Items = [
 // contains the core 3 components
 const optionBar = () => {
     // set to false, will trigger to true on an event which for now is just click
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
 
     // renders the nav items we made and toggles logged in or out
     return (
         <nav className='navbar'>
             <Logo />
             <NavBar items={nav_Items} />
-            <SessionManager isLoggedIn={isLoggedIn} toggleLogin={() => setIsLoggedIn(!isLoggedIn)} />
+            <SessionManager
+                isLoggedIn={isLoggedIn}
+                toggleLogin={() => setIsLoggedIn(!isLoggedIn)}
+            />
         </nav>
-    );
-};
-
+    )
+}
 
 // logo component renders a container div with an image element
 // both the container and the image have their own CSS linked with className
@@ -67,7 +68,7 @@ const Logo = () => (
     <div className='logoContainer'>
         <img src={logo} alt='Maritime Assign Logo' className='maritimeLogo' />
     </div>
-);
+)
 
 // navbar component renders a button for each of the nav items.
 // navbar has its own css
@@ -77,7 +78,7 @@ const NavBar = ({ items }) => (
             <NavButton key={index} item={item} />
         ))}
     </Box>
-);
+)
 
 // button components that are rendered from NavBar
 // renders a button with icon and text with its own css
@@ -88,26 +89,26 @@ const NavButton = ({ item }) => (
             <span className='navButtonText'>{item.text}</span>
         </Link>
     </Button>
-);
+)
 
 // session manager component deals with the authentication login/logout ui
 // isLoggedIn determines the current authentication state
 const SessionManager = ({ isLoggedIn, toggleLogin }) => (
     <div className='sessionContainer'>
-        <Avatar 
-            alt={isLoggedIn ? "User Avatar" : "Guest Avatar"} 
-            src={isLoggedIn ? tempAccountPic : ''} 
-            className='userAvatar' 
+        <Avatar
+            alt={isLoggedIn ? 'User Avatar' : 'Guest Avatar'}
+            src={isLoggedIn ? tempAccountPic : ''}
+            className='userAvatar'
         />
         {isLoggedIn ? (
-            <Link to="/tempProfile" className='navLink'> 
+            <Link to='/tempProfile' className='navLink'>
                 <Button onClick={toggleLogin} className='navButton'>
                     <LogoutIcon className='navBarIcon' />
                     <span className='navButtonText'>Logout</span>
                 </Button>
             </Link>
         ) : (
-            <Link to="/login" className='navLink'> 
+            <Link to='/login' className='navLink'>
                 <Button onClick={toggleLogin} className='navButton'>
                     <LoginIcon className='navBarIcon' />
                     <span className='navButtonText'>Login</span>
@@ -115,5 +116,5 @@ const SessionManager = ({ isLoggedIn, toggleLogin }) => (
             </Link>
         )}
     </div>
-);
-export default optionBar;
+)
+export default optionBar
