6d8a0a29b02c60797e045afa9ea162a02a7a662c
segfault-coredumped <146154806+segfault-coredumped@users.noreply.github.com>
2025-09-05 17:23:05 -0700

    Layout and Error State setup done | Test claiming with account

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,12 +1,67 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
+import { supabase } from './supabase.js'
 
-const JobListing = ({ rowIndex, ...props }) => {
-    const [status, setStatus] = useState(props.open)
+
+const JobListing = ({ rowIndex, handleClaimJob, ...props }) => {
+    const [status, setStatus] = useState(props.open);
+    
+    
+    const [makingClaim, setClaim] = useState(false);
+    const [error, setError] = useState(null);
+
     // make sure the status matches incoming prop
     useEffect(() => {
         setStatus(props.open)
     }, [props.open])
+
+    // Auto-clear error message after 3 seconds
+    useEffect(() => {
+        if (error) {
+            const timer = setTimeout(() => {
+            setError(null);
+            }, 3000);
+            return () => clearTimeout(timer);
+        }
+    }, [error]);
+
+
+    // Claim a job function
+    const claimJob = async () => {
+        setClaim(true);
+        setError(null);
+        const {
+            data: {user},
+            error: userError,
+        } = await supabase.auth.getUser();
+
+        // If claim is made when not logged in
+        if (userError || !user) {
+            setError('You must be logged in to claim a job.');
+            setClaim(false);
+            return;
+        }
+        // Proceed to claim the job
+        const { error: updateError } = await supabase
+            .from('jobs')
+            .update({
+            open: false,
+            fillDate: new Date().toISOString().split('T')[0],
+            claimedBy: user.id,
+            status: 'Filled',
+            claimed_at: new Date().toISOString(),
+        })
+        .eq('id', props.id);
+
+        // Handle any errors during the claim process
+        if (updateError) {
+            setError('Failed to claim the job. Please try again.');
+        }
+        else {
+            handleClaimJob();
+        }
+        setClaimStatus(false);
+    }
 
     const rowClass = rowIndex % 2 === 0 ? 'bg-gray-200' : 'bg-gray-100'
 
@@ -15,54 +70,51 @@ const JobListing = ({ rowIndex, ...props }) => {
 
     return (
         <div className='grid grid-cols-20 w-full text-sm font-mont font-semibold h-fit'>
-            <div
-                className={`col-span-1 px-1 py-2 ${rowClass} items-center justify-center flex`}
-            >
-                <span
-                    className={`${statusColor} px-3 py-1 rounded-md font-semibold`}
-                >
-                    {props.open
-                        ? 'Open'
-                        : `Filled ${props.fillDate ? props.fillDate : ''}`}
-                </span>
-            </div>
-            <div className={`col-span-1  ${cellStyle} ${rowClass}`}>
-                <span>{props.branch1}</span>
-            </div>
-            <div className={`col-span-1  ${cellStyle} ${rowClass}`}>
-                <span>{props.branch2}</span>
+            <div className={`col-span-1 ${cellStyle} ${rowClass}`}>
+                {status ? (
+                    <button
+                    onClick={claimJob}
+                    disabled={makingClaim}
+                    className="bg-green-600 text-white px-3 py-1 rounded hover:bg-green-700 disabled:bg-gray-400 cursor-pointer"
+                    >
+                    {makingClaim ? 'Claiming...' : 'Claim'}
+                    </button>
+                ) : (
+                    <span className="text-red-700 text-sm text-center">
+                    Claimed {props.fillDate ? `on ${props.fillDate}` : ''}
+                    {props.claimedBy ? ` by ${props.claimedBy}` : ''}
+                    </span>
+                )}
             </div>
 
-            <div className={`col-span-2  ${cellStyle} ${rowClass}`}>
-                <span>{props.dateCalled}</span>
-            </div>
-            <div className={`col-span-2  ${cellStyle} ${rowClass}`}>
-                <span>{props.shipName}</span>
-            </div>
-            <div className={`col-span-2  ${cellStyle} ${rowClass}`}>
-                <span>{props.joinDate}</span>
-            </div>
-            <div className={`col-span-1  ${cellStyle} ${rowClass}`}>
-                <span>{props.billet}</span>
-            </div>
-            <div className={`col-span-1  ${cellStyle} ${rowClass}`}>
-                <span>{props.type}</span>
-            </div>
-            <div className={`col-span-1  ${cellStyle} ${rowClass}`}>
-                <span>{props.days}</span>
-            </div>
-            <div className={`col-span-2  ${cellStyle} ${rowClass}`}>
-                <span>{props.location}</span>
-            </div>
-            <div className={`col-span-1  ${cellStyle} ${rowClass}`}>
-                <span>{props.company}</span>
-            </div>
-            <div className={`col-span-2  ${cellStyle} ${rowClass}`}>
-                <span>{props.crewRelieved}</span>
-            </div>
-            <div className={`col-span-3 text-left ${cellStyle} ${rowClass}`}>
-                <span>{props.notes}</span>
-            </div>
+
+            {/* Job Data Grid */}            
+            <div className={`col-span-1  ${cellStyle} ${rowClass}`}><span>{props.branch1}</span></div>
+            <div className={`col-span-1  ${cellStyle} ${rowClass}`}><span>{props.branch2}</span></div>
+            <div className={`col-span-2  ${cellStyle} ${rowClass}`}><span>{props.dateCalled}</span></div>
+            <div className={`col-span-2  ${cellStyle} ${rowClass}`}><span>{props.shipName}</span></div>
+            <div className={`col-span-2  ${cellStyle} ${rowClass}`}><span>{props.joinDate}</span></div>
+            <div className={`col-span-1  ${cellStyle} ${rowClass}`}><span>{props.billet}</span></div>
+            <div className={`col-span-1  ${cellStyle} ${rowClass}`}><span>{props.type}</span></div>
+            <div className={`col-span-1  ${cellStyle} ${rowClass}`}><span>{props.days}</span></div>
+            <div className={`col-span-2  ${cellStyle} ${rowClass}`}><span>{props.location}</span></div>
+            <div className={`col-span-1  ${cellStyle} ${rowClass}`}><span>{props.company}</span></div>
+            <div className={`col-span-2  ${cellStyle} ${rowClass}`}><span>{props.crewRelieved}</span></div>
+            <div className={`col-span-3 text-left ${cellStyle} ${rowClass}`}><span>{props.notes}</span></div>
+            
+            
+                            
+
+            {/*Error display */}
+            {error && (
+                <div className="fixed top-4 right-4 z-50">
+                    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded shadow-md w-80">
+                    <div className="flex items-start justify-between">
+                        <p className="text-sm">{error}</p>
+                    </div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
