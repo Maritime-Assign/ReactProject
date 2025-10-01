fb1cbfbe904683303043508222918cc05e256906
William Scott <wsc0tt@outlook.com>
2025-10-01 14:01:41 -0700

    Fix(ViewBoard):merge phongs code back into ViewBoard.jsx, fix dashboard css

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,88 +13,88 @@ import {
     RiPlayListAddLine,
 } from 'react-icons/ri'
 
-const Dashboard = ({allowedTiles}) => {
+const Dashboard = ({ allowedTiles }) => {
     const dashButton =
-        'group w-60 h-50 flex flex-col items-center justify-center gap-2 rounded-md bg-mebablue-dark hover:bg-mebablue-light transition-colors duration-300 ease-in-out cursor-pointer';
+        'group w-60 h-50 flex flex-col items-center justify-center gap-2 rounded-md bg-mebablue-dark hover:bg-mebablue-light transition-colors duration-300 ease-in-out cursor-pointer'
 
-    console.log('Allowed Tiles (admin):', allowedTiles);
+    console.log('Allowed Tiles (admin):', allowedTiles)
     return (
-    <div className="w-400 h-[calc(100vh-80px)] flex items-center justify-center overflow-y-hidden mx-auto">
-        <div className="w-fit flex flex-col gap-4 p-12 shadow-[0_0_10px_rgba(0,0,0,0.1)] rounded-md">
-        <div className="w-full text-center">
-            <span className="text-3xl font-medium font-mont text-mebablue-dark">
-            Administrator Dashboard
-            </span>
-        </div>
-        <div className="w-full h-[5px] rounded-full bg-mebablue-dark mb-2"></div>
+        <div className='w-full h-[calc(100vh-80px)] flex items-center justify-center overflow-y-hidden mx-auto'>
+            <div className='w-fit flex flex-col gap-4 p-12 shadow-[0_0_10px_rgba(0,0,0,0.1)] rounded-md'>
+                <div className='w-full text-center'>
+                    <span className='text-3xl font-medium font-mont text-mebablue-dark'>
+                        Administrator Dashboard
+                    </span>
+                </div>
+                <div className='w-full h-[5px] rounded-full bg-mebablue-dark mb-2'></div>
 
-        <div className="flex flex-row gap-4 font-mont text-white font-medium text-2xl justify-center items-center">
-            {allowedTiles.includes('manageJobs') && (
-            <div className={dashButton}>
-                <span className="mb-1">Manage Jobs</span>
-                <Link to="/board">
-                <button className="cursor-pointer">
-                    <RiListSettingsLine className="w-[100px] h-[100px]" />
-                </button>
-                </Link>
-            </div>
-            )}
-            {allowedTiles.includes('addJobListing') && (
-            <div className={dashButton}>
-                <span className="mb-1">Add Job Listing</span>
-                <Link to="/addjob">
-                <button className="cursor-pointer">
-                    <RiPlayListAddLine className="w-[100px] h-[100px]" />
-                </button>
-                </Link>
-            </div>
-            )}
-            {allowedTiles.includes('viewJobBoard') && (
-            <div className={dashButton}>
-                <span className="mb-1">View Job Board</span>
-                <Link to="/fsb">
-                <button className="cursor-pointer">
-                    <RiListView className="w-[100px] h-[100px]" />
-                </button>
-                </Link>
-            </div>
-            )}
-        </div>
+                <div className='flex flex-row gap-4 font-mont text-white font-medium text-2xl justify-center items-center'>
+                    {allowedTiles.includes('manageJobs') && (
+                        <div className={dashButton}>
+                            <span className='mb-1'>Manage Jobs</span>
+                            <Link to='/board'>
+                                <button className='cursor-pointer'>
+                                    <RiListSettingsLine className='w-[100px] h-[100px]' />
+                                </button>
+                            </Link>
+                        </div>
+                    )}
+                    {allowedTiles.includes('addJobListing') && (
+                        <div className={dashButton}>
+                            <span className='mb-1'>Add Job Listing</span>
+                            <Link to='/addjob'>
+                                <button className='cursor-pointer'>
+                                    <RiPlayListAddLine className='w-[100px] h-[100px]' />
+                                </button>
+                            </Link>
+                        </div>
+                    )}
+                    {allowedTiles.includes('viewJobBoard') && (
+                        <div className={dashButton}>
+                            <span className='mb-1'>View Job Board</span>
+                            <Link to='/fsb'>
+                                <button className='cursor-pointer'>
+                                    <RiListView className='w-[100px] h-[100px]' />
+                                </button>
+                            </Link>
+                        </div>
+                    )}
+                </div>
 
-        <div className="flex flex-row gap-4 font-mont text-white font-medium text-2xl justify-center items-center">
-            {allowedTiles.includes('viewChanges') && (
-            <div className={dashButton}>
-                <span className="mb-1">View Changes</span>
-                <Link to="/history">
-                <button className="cursor-pointer">
-                    <AiOutlineHistory className="w-[100px] h-[100px]" />
-                </button>
-                </Link>
+                <div className='flex flex-row gap-4 font-mont text-white font-medium text-2xl justify-center items-center'>
+                    {allowedTiles.includes('viewChanges') && (
+                        <div className={dashButton}>
+                            <span className='mb-1'>View Changes</span>
+                            <Link to='/history'>
+                                <button className='cursor-pointer'>
+                                    <AiOutlineHistory className='w-[100px] h-[100px]' />
+                                </button>
+                            </Link>
+                        </div>
+                    )}
+                    {allowedTiles.includes('manageUsers') && (
+                        <div className={dashButton}>
+                            <span className='mb-1'>Manage Users</span>
+                            <Link to='/users-roles'>
+                                <button className='cursor-pointer'>
+                                    <LiaUsersCogSolid className='w-[100px] h-[100px]' />
+                                </button>
+                            </Link>
+                        </div>
+                    )}
+                    {allowedTiles.includes('addUser') && (
+                        <div className={dashButton}>
+                            <span className='mb-1'>Add User</span>
+                            <Link to='/add-user'>
+                                <button className='cursor-pointer'>
+                                    <LiaUserPlusSolid className='w-[100px] h-[100px]' />
+                                </button>
+                            </Link>
+                        </div>
+                    )}
+                </div>
             </div>
-            )}
-            {allowedTiles.includes('manageUsers') && (
-            <div className={dashButton}>
-                <span className="mb-1">Manage Users</span>
-                <Link to="/users-roles">
-                <button className="cursor-pointer">
-                    <LiaUsersCogSolid className="w-[100px] h-[100px]" />
-                </button>
-                </Link>
-            </div>
-            )}
-            {allowedTiles.includes('addUser') && (
-            <div className={dashButton}>
-                <span className="mb-1">Add User</span>
-                <Link to="/add-user">
-                <button className="cursor-pointer">
-                    <LiaUserPlusSolid className="w-[100px] h-[100px]" />
-                </button>
-                </Link>
-            </div>
-            )}
         </div>
-        </div>
-    </div>
-    );
-};
+    )
+}
 export default Dashboard
--- src/pages/ViewBoard.jsx
+++ src/pages/ViewBoard.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Tile from '../components/Tile'
 import Filter from '../components/Filter'
 import fetchJobs from '../components/jobDataAPI' // Assume fetchJobs is an async function
-import { IoArrowBack } from "react-icons/io5";
+import { IoArrowBack } from 'react-icons/io5'
 import { useNavigate } from 'react-router-dom'
 
 function ViewBoard() {
@@ -27,36 +27,59 @@ function ViewBoard() {
     // Update handler
     const handleJobUpdate = (updatedJob) => {
         setJobs((prevJobs) =>
-            prevJobs.map((job) =>
-                job.id === updatedJob.id ? updatedJob : job
-            )
+            prevJobs.map((job) => (job.id === updatedJob.id ? updatedJob : job))
         )
     }
 
     return (
         <div className='w-full pt-4 flex flex-col max-w-[1280px] mx-auto'>
-            {/* header, filter... */}
+            <div className='flex py-4 bg-mebablue-dark rounded-md w-full shadow-xl relative items-center'>
+                <button
+                    onClick={() => navigate(-1)} // navigate back one page
+                    className='bg-mebagold shadow-md rounded-full p-2 absolute left-4 text-2xl text-center text-mebablue-dark'
+                >
+                    <svg
+                        className='w-6 h-6 hover:w-6.5 hover:h-6.5 transition-all ease-in-out text-center items-center justify-center drop-shadow-md'
+                        width='20'
+                        height='20'
+                        viewBox='0 0 24 24'
+                        fill='currentColor'
+                    >
+                        <IoArrowBack />
+                    </svg>
+                </button>
+
+                <div className='flex justify-center py-4 bg-mebablue-dark rounded-md w-full shadow-xl'>
+                    <span className='text-white text-2xl font-medium font-mont'>
+                        Manage Jobs
+                    </span>
+                </div>
+            </div>
             <Filter setView={setView} setFilterOpen={setFilterOpen} />
             <div className='w-full'>
                 {view === 'tile' ? (
                     <div className='grid md:grid-cols-2 grid-cols-1 justify-start place-items-center gap-4'>
-                        {(filterOpen ? jobs.filter((j) => j.open) : jobs).map((job) => (
-                            <Tile
-                                key={job.id}
-                                job={job}
-                                onJobUpdate={handleJobUpdate} // 🔑 pass handler
-                            />
-                        ))}
+                        {(filterOpen ? jobs.filter((j) => j.open) : jobs).map(
+                            (job) => (
+                                <Tile
+                                    key={job.id}
+                                    job={job}
+                                    onJobUpdate={handleJobUpdate} // 🔑 pass handler
+                                />
+                            )
+                        )}
                     </div>
                 ) : (
                     <div className='space-y-4'>
-                        {(filterOpen ? jobs.filter((j) => j.open) : jobs).map((job) => (
-                            <Tile
-                                key={job.id}
-                                job={job}
-                                onJobUpdate={handleJobUpdate} // 🔑 pass handler
-                            />
-                        ))}
+                        {(filterOpen ? jobs.filter((j) => j.open) : jobs).map(
+                            (job) => (
+                                <Tile
+                                    key={job.id}
+                                    job={job}
+                                    onJobUpdate={handleJobUpdate} // 🔑 pass handler
+                                />
+                            )
+                        )}
                     </div>
                 )}
             </div>
