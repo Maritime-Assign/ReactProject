0a18e806bdfd3d5a078dbe730a6456aa24b5253d
William Scott <wsc0tt@outlook.com>
2025-10-01 14:04:58 -0700

    fixed css on other dashboards

--- src/pages/DashboardDisplay.jsx
+++ src/pages/DashboardDisplay.jsx
@@ -11,13 +11,13 @@ import {
 /*
 The viewer dashboard will only access to the dashboard button which allow dashboard viewing.
 */
-const DashboardDisplay = ({allowedTiles}) => {
+const DashboardDisplay = ({ allowedTiles }) => {
     const dashButton =
-        'group w-120 h-102 flex flex-col items-center justify-center gap-2 rounded-md bg-mebablue-dark hover:bg-mebablue-light transition-colors duration-300 ease-in-out overflow-hidden cursor-pointer';
-    
-    console.log('Allowed Tiles (Display):', allowedTiles);
-     return (
-        <div className='w-400 h-[calc(100vh-80px)] flex items-center justify-center overflow-y-hidden mx-auto'>
+        'group w-120 h-102 flex flex-col items-center justify-center gap-2 rounded-md bg-mebablue-dark hover:bg-mebablue-light transition-colors duration-300 ease-in-out overflow-hidden cursor-pointer'
+
+    console.log('Allowed Tiles (Display):', allowedTiles)
+    return (
+        <div className='w-full h-[calc(100vh-80px)] flex items-center justify-center overflow-y-hidden mx-auto'>
             <div className='w-fit flex flex-col gap-4 p-12 shadow-[0_0_10px_rgba(0,0,0,0.1)] rounded-md'>
                 <div className='w-full text-center'>
                     <span className='text-3xl font-medium font-mont text-mebablue-dark'>
