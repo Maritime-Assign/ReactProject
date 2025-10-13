cbfded46294595480cf8f40b99398b802cdba8ac
segfault-coredumped <146154806+segfault-coredumped@users.noreply.github.com>
2025-10-13 16:51:53 -0700

    Add broader coverage for Dashboard Unit tests

--- src/__unitTests__/DashboardDispatch.test.jsx
+++ src/__unitTests__/DashboardDispatch.test.jsx
@@ -31,39 +31,94 @@ const getLinkFromLabel = (labelText) => {
 // and that tiles that should not be present are not in the document
 
 // For the Dispatch dashboard
-test('Dispatch dashboard renders all tiles and buttons work', () => {
-render(
+describe('Dispatch Positive Tests', () => {
+    test('Dispatch dashboard renders all tiles and buttons work', () => {
+        render(
+            <MemoryRouter>
+            <DashboardDispatch
+                allowedTiles={[
+                'manageJobs',
+                'addJobListing',
+                'viewChanges',
+                'viewJobBoard',
+                ]}
+            />
+            </MemoryRouter>
+        )
+
+        expect(screen.getByText(/Manage Jobs/i)).toBeInTheDocument()
+        expect(screen.getByText(/Add Job Listing/i)).toBeInTheDocument()
+        expect(screen.getByText(/View Changes/i)).toBeInTheDocument()
+        expect(screen.getByText(/View Job Board/i)).toBeInTheDocument()
+
+        expect(getLinkFromLabel(/Manage Jobs/i)).toHaveAttribute(
+            'href',
+            '/board'
+        )
+        expect(getLinkFromLabel(/Add Job Listing/i)).toHaveAttribute(
+            'href',
+            '/addjob'
+        )
+        expect(getLinkFromLabel(/View Changes/i)).toHaveAttribute(
+            'href',
+            '/history'
+        )
+        expect(getLinkFromLabel(/View Job Board/i)).toHaveAttribute(
+            'href',
+            '/fsb'
+        )
+        
+    })
+})
+
+// "Negative Tests": These are to test for unexpected behavior when interacting with the dashboard components
+describe('Dispatch Dash Negative Tests', () => {
+test('Dispatch dashboard with no allowed tiles renders no tiles', () => {
+    render(
+        <MemoryRouter>
+            <DashboardDispatch allowedTiles={[]}/>
+        </MemoryRouter>
+    )
+    expect(screen.queryByText(/Manage Jobs/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Add Job Listing/i)).not.toBeInTheDocument()
+
+    expect(screen.queryByText(/View Changes/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/View Job Board/i)).not.toBeInTheDocument()
+})
+
+// 'invalidTile' is included in allowedTiles to verify that the component 
+// ignores unknown tiles and does not render anything unexpected.
+test('Dispatch dashboard does not render invalid tile', () => {
+    render(
+        <MemoryRouter>
+            <DashboardDispatch allowedTiles={['invalidTile', 'manageJobs', 'addJobListing', 'viewChanges','viewJobBoard']} />
+        </MemoryRouter>
+    )
+    // valid tiles renders
+    expect(screen.getByText(/Manage Jobs/i)).toBeInTheDocument()
+    expect(screen.getByText(/Add Job Listing/i)).toBeInTheDocument()
+    expect(screen.getByText(/View Changes/i)).toBeInTheDocument()
+    expect(screen.getByText(/View Job Board/i)).toBeInTheDocument()
+    // invalid tile does not render
+    expect(screen.queryByText(/invalidTile/i)).not.toBeInTheDocument()
+    // Check other tiles are not rendered when they are not part of allowedTiles
+    expect(screen.queryByText(/Manage Users/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Add User/i)).not.toBeInTheDocument()
+})
+
+test('Dispatch Dashboard tiles have valid links', () => {
+  render(
     <MemoryRouter>
-    <DashboardDispatch
-        allowedTiles={[
-        'manageJobs',
-        'addJobListing',
-        'viewChanges',
-        'viewJobBoard',
-        ]}
-    />
+      <DashboardDispatch allowedTiles={['viewChanges', 'viewJobBoard', 'addJobListing', 'manageJobs' ]} />
     </MemoryRouter>
-)
+  )
+  expect(getLinkFromLabel(/Manage Jobs/i)).toHaveAttribute('href', '/board')
+  expect(getLinkFromLabel(/Add Job Listing/i)).toHaveAttribute('href', '/addjob')
+  expect(getLinkFromLabel(/View Job Board/i)).toHaveAttribute('href', '/fsb')
+  expect(getLinkFromLabel(/View Changes/i)).toHaveAttribute('href', '/history')
 
-expect(screen.getByText(/Manage Jobs/i)).toBeInTheDocument()
-expect(screen.getByText(/Add Job Listing/i)).toBeInTheDocument()
-expect(screen.getByText(/View Changes/i)).toBeInTheDocument()
-expect(screen.getByText(/View Job Board/i)).toBeInTheDocument()
+})
 
-expect(getLinkFromLabel(/Manage Jobs/i)).toHaveAttribute(
-    'href',
-    '/board'
-)
-expect(getLinkFromLabel(/Add Job Listing/i)).toHaveAttribute(
-    'href',
-    '/addjob'
-)
-expect(getLinkFromLabel(/View Changes/i)).toHaveAttribute(
-    'href',
-    '/history'
-)
-expect(getLinkFromLabel(/View Job Board/i)).toHaveAttribute(
-    'href',
-    '/fsb'
-)
+
+
 })
